7b96ff3bc03f1e323a9da8fbbb4a4feddabcbbf3
julien.reichenbch <julien.reichenbach@hevs.ch>
2020-07-24 14:40:38 +0200

    - CORS fixed for prod server

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,10 +67,6 @@ export default {
     '@nuxtjs/style-resources',
     'nuxt-leaflet',
   ],
-  /* Env variables */
-  env: {
-    baseUrl: 'localhost:3333/api',
-  },
   /* i18n config */
   i18n: {
     vueI18nLoader: false,
